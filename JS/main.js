ffba91a5e4316c8a0748514f82fbeea27a8f8a89
omckeen01 <111658388+omckeen01@users.noreply.github.com>
2022-10-13 14:26:52 -0600

    h

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+const storyText = 'It was a bright sunny summer day, so :insertx: went for a swim at the beach. When they got to :inserty:, they found A MILLION seashells, then :insertz:. Then a shark swam up! — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Ben', 'The Shark', 'Santa Claus'];
+const insertY = ['Catalina Island', 'Boulder Reservoir', 'Crystal Cove'];
+const insertZ = ['jumped with glee', 'rolled in the sand', 'found a crab'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  const xItem = randomValueFromArray(insertX);
+  const yItem = randomValueFromArray(insertY);
+  const zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(':insertx:',xItem);
+  newStory = newStory.replaceAll(':inserty:',yItem);
+  newStory = newStory.replaceAll(':insertz:',zItem);
+
+  if (customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replaceAll('Ben', name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300*0.0714286)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('bright sunny summer day', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}

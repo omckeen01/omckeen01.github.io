d84725a52f35f3186328480b11830259c020ed22
omckeen01 <111658388+omckeen01@users.noreply.github.com>
2022-11-08 10:36:15 -0700

    h

--- assignments/week12.js
+++ assignments/week12.js
@@ -1,4 +1,51 @@
-const button = document.querySelector('.button');
+
+
+function check(){
+    console.log('test');
+}
+
+function submit(){
+    alert(output.textContent);
+}
+
+function reset(){
+    outputInt = 0;
+    output.textContent = outputInt;   
+}
+
+function plusRunner(){
+    plus(outputInt);
+}
+
+function plus() {
+    outputInt +=1;
+    output.textContent = outputInt;
+
+}
+
+function random() {
+    outputInt = randomNumber(0,9999999999);
+    output.textContent = outputInt;
+}
+
+function randomNumber(min,max){ 
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+const output = document.querySelector('.output')
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+const minusButton = document.querySelector('.minus-button').addEventListener('click', check);
+const plusButton = document.querySelector('.plus-button').addEventListener('click', plusRunner);
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+
+
+
+/*const button = document.querySelector('.button');
 const output = document.querySelector('.output');
 let phone_content = document.querySelector('.phone');
 
@@ -6,4 +53,17 @@ button.addEventListener('click', updateOutput);
 
 function updateOutput() {
     output.textContent = phone_content.value;
+    alert (phone_content.value);
 }
+*/
+
+var slider = document.getElementById("myRange");
+
+var sliderOutput = document.querySelector(".slider-output");
+//output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+sliderOutput.oninput = function() {
+  sliderOutput.innerHTML = this.value;
+}
+

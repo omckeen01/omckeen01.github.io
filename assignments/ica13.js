c9585a077dcd7512d375b427e0d7b58db1dd4415
omckeen01 <111658388+omckeen01@users.noreply.github.com>
2022-11-15 09:55:58 -0700

    h

---
+++ assignments/ica13.js
@@ -0,0 +1,48 @@
+const aliceTumbling = [
+  { transform: 'rotate(0) scale(1)' },
+  { transform: 'rotate(360deg) scale(0)' }
+];
+
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: 'forwards'
+}
+
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+
+function firstAlice (){
+  return new Promise (function(resolve){
+      setTimeout(function(){
+          alice1.animate(aliceTumbling, aliceTiming);
+          resolve()
+      }, 250)
+
+  })
+}
+
+function secondAlice (){
+  return new Promise (function(resolve){
+      setTimeout(function(){
+          alice2.animate(aliceTumbling, aliceTiming);
+          resolve()
+      }, 2000)
+
+  })
+}
+
+function thirdAlice (){
+  return new Promise (function(resolve){
+      setTimeout(function(){
+          alice3.animate(aliceTumbling, aliceTiming);
+          resolve()
+      }, 4000)
+
+  })
+}
+firstAlice();
+secondAlice();
+thirdAlice();
